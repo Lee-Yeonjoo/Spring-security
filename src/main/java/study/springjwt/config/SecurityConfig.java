d5555ebe8361e332bbd51b357c7f3c2c0bfe235c
Yeonjoo Lee <joos0207@naver.com>
2024-05-18 18:20:34 +0900

    로그인 필터 구현

--- src/main/java/study/springjwt/config/SecurityConfig.java
+++ src/main/java/study/springjwt/config/SecurityConfig.java
@@ -1,16 +1,30 @@
 package study.springjwt.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import study.springjwt.jwt.LoginFilter;
 
 @Configuration //스프링이 configuration으로 관리
 @EnableWebSecurity //시큐리티를 위한 config
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean //로그인 필터의 인자로 auth매니저를 넣어주기 위해
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() { //비밀번호를 해시 암호화 시킬 수 있다.
@@ -39,6 +53,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll() //해당 경로에선 모든 권한 허용
                         .requestMatchers("/admin").hasRole("ADMIN") //해당 경로에선 admin만 권한 허용
                         .anyRequest().authenticated()); //이외의 요청에서는 권한 허용x
+
+        //커스텀 필터 등록
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class); //UsernamePasswordAuthenticationFilter를 대체하므로 그 자리에 등록
 
         //세션 설정 - JWT방식은 세션을 stateless상태로 설정해야한다! 가장 중요!
         http
---
+++ src/main/java/study/springjwt/jwt/LoginFilter.java
@@ -0,0 +1,46 @@
+package study.springjwt.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter { //회원 검증을 위한 커스텀 필터 -> SecurityConfig에 등록해야한다.
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        //클라이언트 요청에서 username과 password를 꺼낸다.
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        System.out.println("username = " + username); //요청에서 잘 추출했는지 확인
+
+        //꺼낸 값으로 인증 진행하기 위해 필터가 AuthenticationManager에게 username과 password를 넘겨야 되는데 이때 dto로 넘겨야 한다.
+        //dto는 UsernamePasswordAuthenticationToken
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        //AuthenticationManager에게 전달 -> 이제 매니저가 검증 진행
+        return authenticationManager.authenticate(authToken);
+    }
+
+    //검증 성공 시 실행되는 메소드
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    //검증 실패 시 실행되는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+}
